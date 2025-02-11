322694cea52a48742e6db7df242568b26a3b2554
ashish-tiwari98 <ashishsark98@gmail.com>
2025-02-11 18:39:06 +0530

    Initial frontend setup with Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-red-500 p-10 border-4 border-black shadow-xl rounded-lg text-white">
+    <h1 className="text-2xl">Tailwind CSS Test</h1>
+    <p className="mt-4">This is a test paragraph.</p>
+    </div>
+  );
+}
+
+export default App;
